00490047862c56ce1f99def6818383f1d4d4e202
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-07-22 10:49:08 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int number=8;
+{
+printf("enter the integer");
+}
+if(number %2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+return 0;
+}
