64c346be672acbd3c5d842cef69316a63d6d5bc3
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-07-22 10:29:09 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,1 +1,18 @@
-ajfdhb
+#include<stdio.h>
+int main()
+{
+int num=5;
+if(num>0)
+{
+printf("the number is positive");
+}
+else if(num<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("number is zero");
+}
+return 0;
+}
