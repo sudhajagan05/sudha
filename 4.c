e7943a34bd3e3c11956de3a98f4e1d856cf6be7c
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-07-22 15:11:16 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+char c;
+{
+printf("enter the character");
+scanf("%c",&c);
+}
+if((c>='a' && c<='z')||(c>='a' && c<='z'))
+{
+printf("%c is an  alphapet",c);
+}
+else
+{
+printf("%c is not alphapet",c);
+}
+return 0;
+}
