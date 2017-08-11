83c5b817e57cef24b5a2f1b69e1799a8be8f53b0
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-08-11 16:24:53 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -5,8 +5,6 @@ int main()
     char    str[100];
     int countDigits,countAlphabet,countSpecialChar,countSpaces;
     int counter;
- 
-    //assign all counters to zero
     countDigits=countAlphabet=countSpecialChar=countSpaces=0;
  
     printf("Enter a string: ");
@@ -14,8 +12,7 @@ int main()
  
     for(counter=0;str[counter]!=NULL;counter++)
     {
- 
-        if(str[counter]>='0' && str[counter]<='9')
+     if(str[counter]>='0' && str[counter]<='9')
             countDigits++;
         else if((str[counter]>='A' && str[counter]<='Z')||(str[counter]>='a' && str[counter]<='z'))
             countAlphabet++;
