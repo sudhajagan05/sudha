e597c28423ef8be1920d298014ff88ac79262cf9
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-08-11 18:21:13 +0530

    Update 48.c

--- 48.c
+++ 48.c
@@ -2,27 +2,27 @@
 
 int main()
 {
-    int n, i;
+    int u, i;
     float num[100], sum = 0.0, average;
 
     printf("Enter the numbers of elements: ");
-    scanf("%d", &n);
+    scanf("%d", &u);
 
-    while (n > 100 || n <= 0)
+    while (u > 100 || u <= 0)
     {
         printf("Error! number should in range of (1 to 100).\n");
         printf("Enter the number again: ");
         scanf("%d", &n);
     }
 
-    for(i = 0; i < n; ++i)
+    for(i = 0; i < u; ++i)
     {
         printf("%d. Enter number: ", i+1);
         scanf("%f", &num[i]);
         sum += num[i];
     }
 
-    average = sum / n;
+    average = sum / u;
     printf("Average = %.2f", average);
 
     return 0;
