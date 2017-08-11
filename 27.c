76afd2d470da4ea5926150b6e04c76bf9062cb04
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-08-11 16:32:53 +0530

    Update 27.c

--- 27.c
+++ 27.c
@@ -6,10 +6,8 @@ int main()
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
 
-    // maximum number between n1 and n2 is stored in minMultiple
     minMultiple = (n1>n2) ? n1 : n2;
 
-    // Always true
     while(1)
     {
         if( minMultiple%n1==0 && minMultiple%n2==0 )
