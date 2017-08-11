12678c7fc53c6c9fc95d5fafda0a5398403fb4d8
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-08-11 18:15:53 +0530

    Update 43.c

--- 43.c
+++ 43.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 void main(void)
 {
-  char str1[25],str2[25];
+  char s1[25],s2[25];
   int i=0,j=0;
   printf("\nEnter First String:");
-  gets(str1);
+  gets(s1);
   printf("\nEnter Second String:");
-  gets(str2);
-  while(str1[i]!='\0')
+  gets(s2);
+  while(s1[i]!='\0')
   i++;
-  while(str2[j]!='\0')
+  while(s2[j]!='\0')
   {
-    str1[i]=str2[j];
+    s1[i]=s2[j];
     j++;
     i++;
   }
-  str1[i]='\0';
-  printf("\nConcatenated String is %s",str1);
+  s1[i]='\0';
+  printf("\nConcatenated String is %s",s1);
 }
