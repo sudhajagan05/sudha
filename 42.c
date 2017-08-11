76dc23c18b04e8b65ec0cc5dd457a73d6e7e5d64
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-08-11 17:50:15 +0530

    Update 42.c

--- 42.c
+++ 42.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
  
 int main() {
-   char str1[30], str2[30];
+   char s1[30], s2[30];
    int i;
  
    printf("\nEnter two strings :");
-   gets(str1);
-   gets(str2);
+   gets(s1);
+   gets(s2);
  
    i = 0;
-   while (str1[i] == str2[i] && str1[i] != '\0')
+   while (s1[i] == s2[i] && s1[i] != '\0')
       i++;
-   if (str1[i] > str2[i])
-      printf("str1 > str2");
-   else if (str1[i] < str2[i])
-      printf("str1 < str2");
+   if (s1[i] > s2[i])
+      printf("s1 > s2");
+   else if (s1[i] < s2[i])
+      printf("s1 < s2");
    else
-      printf("str1 = str2");
+      printf("s1 = s2");
  
    return (0);
 }
