b7cb47c0c5a41256d98341333677a6accb303d29
sudhajagan05 <30342648+sudhajagan05@users.noreply.github.com>
2017-08-11 17:23:31 +0530

    Update 39.c

--- 39.c
+++ 39.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    double n, n2, n3;
+    double num, num2, num3;
 
     printf("Enter three different numbers: ");
-    scanf("%lf %lf %lf", &n, &n2, &n3);
+    scanf("%lf %lf %lf", &num, &num2, &num3);
 
-    if( n>=n2 && n>=n3 )
-        printf("%.2f is the largest number.", n);
+    if( num>=num2 && num>=num3 )
+        printf("%.2f is the largest number.", num);
 
-    if( n2>=n && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
+    if( num2>=num && num2>=num3 )
+        printf("%.2f is the largest number.", num2);
 
-    if( n3>=n && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
+    if( num3>=num && num3>=num2 )
+        printf("%.2f is the largest number.", num3);
 
     return 0;
 }
